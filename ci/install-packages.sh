41014167a2b057037c4d24e76474d544e3eaba67
Kai Mast <kaimast@cs.cornell.edu>
2019-06-04 16:04:35 -0700

    Use older clang

--- ci/install-packages.sh
+++ ci/install-packages.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# For g++-9
+# For g++-9 #TODO remove once we update to bionic
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 
 # We need a more recent meson too
@@ -8,4 +8,5 @@ sudo add-apt-repository ppa:jonathonf/meson -y
 
 sudo apt-get update
 
-sudo apt-get install meson build-essential git g++-9 libgtest-dev libgflags-dev pkg-config make clang-7 clang-tidy-7 -y
+# TODO move to newer clang once we update to bionic
+sudo apt-get install meson build-essential git g++-9 libgtest-dev libgflags-dev pkg-config make clang-tidy-6.0 -y
