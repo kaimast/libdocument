908bba7ef23ea922147db2def57fe9296a5d5c43
Kai Mast <kaimast@cs.cornell.edu>
2019-06-04 17:52:17 -0700

    Use gcc-9

--- ci/setup-prereq.sh
+++ ci/setup-prereq.sh
@@ -4,8 +4,8 @@ WORKDIR=$HOME/prereq
 INSTALL_DIR=$HOME/local
 
 BUILDTYPE=release
-export CC=gcc-8
-export CXX=g++-8
+export CC=gcc-9
+export CXX=g++-9
 
 export LIBRARY_PATH=/usr/lib:/usr/local/lib:$INSTALL_DIR/lib:$INSTALL_DIR/lib/x86_64-linux-gnu
 export LD_LIBRARY_PATH=/usr/lib:/usr/local/lib:$INSTALL_DIR/lib:$INSTALL_DIR/lib/x86_64-linux-gnu
