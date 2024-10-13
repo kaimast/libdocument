563f8db0385078f9420d76ea0dd4ed44a7634f76
Kai Mast <kai@kaimast.com>
2024-10-12 23:03:07 -0700

    Build fixes

--- scripts/setup-prereq.sh
+++ scripts/setup-prereq.sh
@@ -6,8 +6,9 @@ BUILDTYPE=release
 export LIBRARY_PATH=/usr/lib:/usr/local/lib:$INSTALL_DIR/lib:$INSTALL_DIR/lib/x86_64-linux-gnu
 export LD_LIBRARY_PATH=/usr/lib:/usr/local/lib:$INSTALL_DIR/lib:$INSTALL_DIR/lib/x86_64-linux-gnu
 
+pushd deps
 echo "Building bitstream"
-cd deps/bitstream
+pushd bitstream
 meson build -Dbuildtype=$BUILDTYPE --prefix=$INSTALL_DIR
 cd build
 ninja install -v
